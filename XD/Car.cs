574a3ed2fa8cfcaf3a7027052ab7ff80b1219332
Stefania1703 <116831218+Stefania1703@users.noreply.github.com>
2024-01-10 08:06:58 +0200

    proiect XD

---
+++ XD/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD
+{
+    public class Car:Vehicul
+    {
+        private string color;
+        private string name;
+        private int maxSpeed;
+        private int year;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                try
+                {
+                    if (value < 1980)
+                    {
+                        throw new Exception("Please pass a correct value");
+                    }
+                    else
+                    {
+                        year = value;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Error "+ ex.Message);
+                }
+                
+            }
+        }
+        public Car(string colorName, string nameName, int maxSpeedValue, int yearValue)
+        {
+            Color = colorName;
+            Name = nameName;
+            maxSpeed = maxSpeedValue;
+            Year = yearValue;
+        }
+        public Car(string colorName, string nameName, int maxSpeedValue, int yearValue, string eng_type)
+        {
+            Color = colorName;
+            Name = nameName;
+            maxSpeed = maxSpeedValue;
+            Year = yearValue;
+            engine_type = eng_type;
+        }
+        public Car(string nameName)
+        {
+            name = nameName;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine("is going at the speed of " + maxSpeed);
+        }
+
+    }
+}
---
+++ XD/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120, "electric");
+            Car Volvo = new Car("XC-90");
+
+            Console.WriteLine(Ford.Name);
+            Console.WriteLine(Ford.Color);
+            Console.WriteLine(Ford.Year);
+            Console.WriteLine(Ford.engine_type);
+            Ford.fullThrottle();
+
+            Console.WriteLine(Opel.Name);
+            Console.WriteLine(Opel.Color);
+            Console.WriteLine(Opel.Year);
+            Console.WriteLine(Volvo.engine_type);
+            Opel.fullThrottle();
+            Opel.Honk();
+
+            Console.WriteLine("Enter Volvo's age:");
+            try
+            {
+                Volvo.Year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Volvo age is:" + Volvo.Year);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
